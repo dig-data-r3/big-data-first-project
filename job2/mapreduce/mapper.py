7bce61b00851a2d3600e7026896c6f5636a79d6a
Alex <alexandru.pavel.94@gmail.com>
2021-05-06 10:33:19 +0200

    job2 mapper (first version, to test)

---
+++ job2/mapreduce/mapper.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""mapper.py"""
+
+import sys
+import csv
+from datetime import datetime
+
+ticker_to_sector = {}
+
+# process the historical stocks database (already cleaned from null sectors) before the join
+with open('historical_stocks_clean.csv') as hs_file:
+    # csv.reader reads the fields with a comma inside " " correctly (company name)
+    csv_reader = csv.reader(hs_file, delimiter=',')
+    is_first_line = True
+    for row in csv_reader:
+        if is_first_line:
+            # ignore csv header information
+            is_first_line = False
+        else:
+            ticker, _, _, sector, _ = row
+            ticker_to_sector[ticker] = sector
+
+# job2 requires a specific time frame to consider
+START_YEAR = 2009
+END_YEAR = 2018
+
+is_first_line = True
+
+# read lines from STDIN (historical_stock_prices dataset)
+for row in sys.stdin:
+    # ignores the first row, which contains column names
+    if is_first_line:
+        is_first_line = False
+        continue
+
+    # split the current row into fields (ignoring not needed ones)
+    ticker, _, closePrice, _, _, _, volume, date = row.strip().split(',')
+    date = datetime.strptime(date, '%Y-%m-%d').date()
+
+    # write the separated fields to standard output, applying the necessary filtering
+    if START_YEAR <= date.year <= END_YEAR:
+        # the join adds a column sector
+        sector = ticker_to_sector[ticker]
+        print('{}\t{}\t{}\t{}\t{}'.format(
+            # shuffle and sort could be useful on these first three columns
+            sector,
+            ticker,
+            date,
+            closePrice,
+            volume))
