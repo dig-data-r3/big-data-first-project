9a643de1a672983e377f5aa8b7cccfcca70dbddf
Alex <alexandru.pavel.94@gmail.com>
2021-05-07 11:37:18 +0200

    saves each ticker data (per year and sector) into a dict

--- job2/mapreduce/reducer.py
+++ job2/mapreduce/reducer.py
@@ -4,16 +4,37 @@
 import sys
 from datetime import datetime
 
-# maps each (sector, year) to the required values to calculate
+# map each ticker for each sector and year to the values required to compute result
+#   tickerDataBySectorYear[('AAPL', 'TECH', 2012)] = {
+#       'first_close_date': 2012-01-01,
+#       'first_close_value': 50.5,
+#       'last_close_date': 2012-12-31,
+#       'last_close_value': 240,
+#       'total_volume': 300000}
+# after loading all the values it's useful to compute the percentage variation for the next step
+tickerDataBySectorYear = {}
+
+# structure to save the aggregated values from the previous one
+#   aggregatedSectorYearData[('TECH', 2012)] = {
+#       'sum_initial_close': 4000,
+#       'sum_final_close': 6000,
+#       'max_perc_var_ticker': 'AAPL',
+#       'max_perc_var_value': 75,
+#       'max_total_volume_ticker': 'AAPL',
+#       'max_total_volume_value': 3000000
+#       }
+aggregatedSectorYearData = {}
+
+# map each (sector, year) to the required values to calculate
 # for example:
-#   {('FINANCE', 2010): {
+#   results = {('FINANCE', 2010): {
 #       'year_quot_perc_var': 13.6,
 #       'max_perc_incr_ticker': 'AMZN',
 #       'max_perc_incr_value': 25.3,
 #       'max_vol_incr_ticker': 'AAPL',
-#       'max_vol_incr_value': 2000}}
-results = {}
-
+#       'max_vol_incr_value': 2000}, ...}
+# this is not needed, to provide results iterate over previous structure and compute the perc var
+#results = {}
 
 
 # input comes from STDIN
@@ -32,55 +53,55 @@ for line in sys.stdin:
         # if one of the numerical fields is not formatted correctly
         continue
 
-    # if the (sector, year) hasn't been seen before, initialize its values
-    if (sector, date.year) not in results:
-        #   {('FINANCE', 2010): {
-        #       'year_quot_perc_var': 13.6,
-        #       'max_perc_incr_ticker': 'AMZN',
-        #       'max_perc_incr_value': 25.3,
-        #       'max_vol_incr_ticker': 'AAPL',
-        #       'max_vol_incr_value': 2000}}
+    # save (in memory) the info of each ticker per year and sector (inefficient)
+    if (ticker, sector, date.year) not in tickerDataBySectorYear:
+        newTicker = {'first_close_date': date,
+                     'first_close_value': ticker,
+                     'last_close_date': date,
+                     'last_close_value': ticker,
+                     'total_volume': volume}
+        tickerDataBySectorYear[(ticker, sector, date.year)] = newTicker
+    else:
+        currTicker = tickerDataBySectorYear[(ticker, sector, date.year)]
+        if date < currTicker['first_close_date']:
+            currTicker['first_close_date'] = date
+            currTicker['first_close_value'] = ticker
+        if date > currTicker['last_close_date']:
+            currTicker['last_close_date'] = date
+            currTicker['last_close_value'] = ticker
+        currTicker['total_volume'] += volume
 
-        results[(sector, date.year)] = {
-            'first_quot_date': date,
-            'last_quot_date': date,
-            'first_quot_price': closePrice,
-            'last_quot_price': closePrice,
-            'perc_var': 0,
-            'min_price': minPrice,
-            'max_price': maxPrice
-        }
+# structure to save the aggregated values from the previous one
+#   aggregatedSectorYearData[('TECH', 2012)] = {
+#       'sum_initial_close': 4000,
+#       'sum_final_close': 6000,
+#       'max_perc_var_ticker': 'AAPL',
+#       'max_perc_var_value': 75,
+#       'max_total_volume_ticker': 'AAPL',
+#       'max_total_volume_value': 3000000
+#       }
 
-    currTicker = results[ticker]
+# aggregate the single ticker and year data by sector
+for (ticker, sector, year) in tickerDataBySectorYear:
+    if (sector, year) not in aggregatedSectorYearData:
 
-    # update the current input ticker
-    if date < results[ticker]['first_quot_date']:
-        currTicker['first_quot_date'] = date
-        currTicker['first_quot_price'] = closePrice
-    if date > results[ticker]['last_quot_date']:
-        currTicker['last_quot_date'] = date
-        currTicker['last_quot_price'] = closePrice
-    if minPrice < results[ticker]['min_price']:
-        currTicker['min_price'] = minPrice
-    if maxPrice > results[ticker]['max_price']:
-        currTicker['max_price'] = maxPrice
 
-# sort the results from the most recent to old quotation dates
-sortedResults = sorted(results.items(),
-                       # result is in the format ('TickerName', {'min': 1, 'max': 2}), a tuple
-                       key=lambda single_result: single_result[1]['last_quot_date'],
-                       reverse=True)
-
-# result is in the format ('TickerName', {'min': 1, 'max': 2}), a tuple
-for result in sortedResults:
-    tickerName = result[0]
-    tickerResults = result[1]
-    tickerResults['perc_var'] = (tickerResults['last_quot_price'] - tickerResults['first_quot_price']) / \
-        tickerResults['first_quot_price'] * 100
-    print('{:<10s}\t{}\t{}\t{:>25}%\t{:<20}\t{:<20}'.format(
-        tickerName,
-        tickerResults['first_quot_date'],
-        tickerResults['last_quot_date'],
-        tickerResults['perc_var'],
-        tickerResults['min_price'],
-        tickerResults['max_price']))
+# # sort the results from the most recent to old quotation dates
+# sortedResults = sorted(results.items(),
+#                        # result is in the format ('TickerName', {'min': 1, 'max': 2}), a tuple
+#                        key=lambda single_result: single_result[1]['last_quot_date'],
+#                        reverse=True)
+#
+# # result is in the format ('TickerName', {'min': 1, 'max': 2}), a tuple
+# for result in sortedResults:
+#     tickerName = result[0]
+#     tickerResults = result[1]
+#     tickerResults['perc_var'] = (tickerResults['last_quot_price'] - tickerResults['first_quot_price']) / \
+#         tickerResults['first_quot_price'] * 100
+#     print('{:<10s}\t{}\t{}\t{:>25}%\t{:<20}\t{:<20}'.format(
+#         tickerName,
+#         tickerResults['first_quot_date'],
+#         tickerResults['last_quot_date'],
+#         tickerResults['perc_var'],
+#         tickerResults['min_price'],
+#         tickerResults['max_price']))
