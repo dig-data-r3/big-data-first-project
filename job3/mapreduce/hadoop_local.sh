2cbb990faebb189925c7c9faed8b587d5c81b623
Alex <alexandru.pavel.94@gmail.com>
2021-05-08 00:19:45 +0200

    mapred streaming orders months correctly

--- job3/mapreduce/hadoop_local.sh
+++ job3/mapreduce/hadoop_local.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-#hdfs dfs -rm -r output/job3_mapreduce/
-hdfs dfs -rm -r tmp/
+hdfs dfs -rm -r output/job3_mapreduce/
+#hdfs dfs -rm -r tmp/job3_mapreduce
 cd "$(dirname "$0")" || exit
-  # -D mapreduce.job.reduces=2 \
-  #- D stream.num.map.output.key.fields=3 \
-  # -D mapreduce.partition.keypartitioner.options=-k1,1 \
-mapred streaming \
-  -D mapreduce.job.reduces=1 \
-  -files firstMapper.py,firstReducer.py \
-  -mapper firstMapper.py \
-  -reducer firstReducer.py \
-  -input input/historical_stock_prices.csv \
-  -output tmp/job3_mapreduce \
-#&& \
+#  -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
+#  -D stream.num.map.output.key.fields=2 \
+#  -D mapreduce.partition.keycomparator.options=-k1,2n \
 #mapred streaming \
-#  -files secondMapper.py,secondReducer.py \
-#  -mapper secondMapper.py \
-#  -reducer secondReducer.py \
-#  -input tmp/part-* \
-#  -output output/job3_mapreduce
+#  -files firstMapper.py,firstReducer.py \
+#  -mapper firstMapper.py \
+#  -reducer firstReducer.py \
+#  -input input/historical_stock_prices.csv \
+#  -output tmp/job3_mapreduce \
+#&& \
+mapred streaming \
+  -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
+  -D stream.num.map.output.key.fields=2 \
+  -D mapreduce.partition.keycomparator.options=-k2,2n \
+  -files secondMapper.py,secondReducer.py \
+  -mapper secondMapper.py \
+  -reducer secondReducer.py \
+  -input tmp/job3_mapreduce/part-* \
+  -output output/job3_mapreduce
