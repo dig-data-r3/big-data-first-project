49783d77a70eca02d1d20f2dedb4a0f7d4b8d123
Alex <alexandru.pavel.94@gmail.com>
2021-05-08 12:11:59 +0200

    restored the full chain of mapreduces in job3

--- job3/mapreduce/hadoop_local.sh
+++ job3/mapreduce/hadoop_local.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 hdfs dfs -rm -r output/job3_mapreduce/
-#hdfs dfs -rm -r tmp/job3_mapreduce
+hdfs dfs -rm -r tmp/job3_mapreduce/
 cd "$(dirname "$0")" || exit
 #  -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
 #  -D stream.num.map.output.key.fields=2 \
 #  -D mapreduce.partition.keycomparator.options=-k1,2n \
-#mapred streaming \
-#  -files firstMapper.py,firstReducer.py \
-#  -mapper firstMapper.py \
-#  -reducer firstReducer.py \
-#  -input input/historical_stock_prices.csv \
-#  -output tmp/job3_mapreduce \
-#&& \
+mapred streaming \
+  -files firstMapper.py,firstReducer.py \
+  -mapper firstMapper.py \
+  -reducer firstReducer.py \
+  -input input/historical_stock_prices.csv \
+  -output tmp/job3_mapreduce \
+&& \
 mapred streaming \
   -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
   -D stream.num.map.output.key.fields=2 \
