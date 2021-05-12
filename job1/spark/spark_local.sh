003baa639dc0cff0a549c5970d0b13e43cc9c353
Alex <alexandru.pavel.94@gmail.com>
2021-05-12 18:06:04 +0200

    script now deletes the output folder before starting

--- job1/spark/spark_local.sh
+++ job1/spark/spark_local.sh
@@ -1,5 +1,17 @@
-$SPARK_HOME/bin/spark-submit \
-  --master yarn \
+#!/bin/bash
+
+# hdfs dfs -rm -r output/job1_spark/
+
+#"$SPARK_HOME"/bin/spark-submit \
+#  --master yarn \
+#  ./job1/spark/spark_job1.py \
+#  --input_path hdfs:///user/alex/input/historical_stock_prices.csv \
+#  --output_path hdfs:///user/alex/output/job1_spark
+
+rm -r ./results/job1_spark/
+
+"$SPARK_HOME"/bin/spark-submit \
+  --master local[4] \
   ./job1/spark/spark_job1.py \
-  --input_path hdfs:///user/alex/input/historical_stock_prices.csv \
-  --output_path hdfs:///user/alex/output/job1_spark
+  --input_path file:///home/alex/git/big-data-first-project/dataset/historical_stock_prices.csv \
+  --output_path file:///home/alex/git/big-data-first-project/results/job1_spark
