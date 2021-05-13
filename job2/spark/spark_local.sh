27b90db37a425c7cd5868932fe53ec6a532b8781
Alex <alexandru.pavel.94@gmail.com>
2021-05-13 11:54:46 +0200

    reads two input files

--- job2/spark/spark_local.sh
+++ job2/spark/spark_local.sh
@@ -13,5 +13,6 @@ rm -r ./results/job2_spark/
 "$SPARK_HOME"/bin/spark-submit \
   --master local[4] \
   ./job2/spark/spark_job2.py \
-  --input_path file:///home/alex/git/big-data-first-project/dataset/historical_stock_prices.csv \
+  --input_hsp file:///home/alex/git/big-data-first-project/dataset/historical_stock_prices.csv \
+  --input_hs file:///home/alex/git/big-data-first-project/dataset/historical_stocks_clean.csv \
   --output_path file:///home/alex/git/big-data-first-project/results/job2_spark
