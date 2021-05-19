bffb53fdbb98c827d438a96bc3ea674cb6c8e7b4
Alex <alexandru.pavel.94@gmail.com>
2021-05-19 11:25:20 +0200

    adds error info to run correctly

--- run_job_local.sh
+++ run_job_local.sh
@@ -5,6 +5,17 @@
 # application = hadoop, hive, spark
 # dataset_size = 256, 512, 1024, default=2048MB (empty filename), 4096
 
+if [ $# -eq 0 ]; 
+then
+	echo "Error: Too few arguments."
+    echo "Usage: ./run_job_local n_job application [dataset_size (MB)]"
+    echo "n_job = 1, 2, 3"
+    echo "application = hadoop, hive, spark"
+    echo "dataset_size = 256, 512, 1024, default=2048MB (empty filename), 4096"
+    exit 1
+fi
+
+
 if [ $2 == 'hive' ];
 then
 	time hive -hivevar DATASET_NAMESIZE=historical_stock_prices$3 --f ./job$1/$2/job$1.hql
